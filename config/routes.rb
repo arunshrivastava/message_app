3af99ee7d2db0bfc9a6b9611f1f072bf742625e4
ARUN SHRIVASTAVA <arunshrivastava2017@EMAIL.com>
2018-08-02 12:16:00 +0530

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+
+  devise_for :users
+  resources :messages do
+     resources :comments
+  end 
+   root to: "messages#index"
+
+
+
+  
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
